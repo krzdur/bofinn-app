f07b4cb5df17768cc9c89c0f29edaa49cf63e6aa
krzysztof <krzdur@gmail.com>
2025-05-04 22:55:03 +0200

    handle "sending" emails (send to stdout)

--- webapp/contact/views.py
+++ webapp/contact/views.py
@@ -1,9 +1,38 @@
-from django.shortcuts import render
+from django.core.mail import send_mail
+from django.shortcuts import render, redirect
+from django.template.loader import render_to_string
 
 from .forms import ContactForm
 
 def contact(request):
-    form = ContactForm()
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            content = form.cleaned_data['content']
+
+            message = render_to_string('emails/message.html', {
+                'name': name,
+                'email': email,
+                'message': content
+            }
+                                    )
+            send_mail(
+                subject='New message in the contact form',
+                message='Hey BOFINNer, you\'ve got a new message through the contact form!',
+                from_email='noreply@bofinn.com',
+                recipient_list=['contact@boffin.com'],
+                html_message=message
+            )
+
+            return redirect('thankyou')
+
+    else:
+        form = ContactForm()
+
+
     return render(request, 'contact.html', {'form': form})
 
 
--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -136,3 +136,6 @@ MEDIA_ROOT = BASE_DIR / 'properties/media/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email backend set to stdout
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
