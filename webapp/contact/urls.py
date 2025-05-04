a9f2feb71efd82c22168faa727aeaaf14427c068
krzysztof <krzdur@gmail.com>
2025-05-04 21:53:26 +0200

    add a form and thank you page

--- webapp/contact/urls.py
+++ webapp/contact/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.contact, name='contact')
+    path('', views.contact, name='contact'),
+    path('thankyou', views.thankyou, name='thankyou')
 ]
