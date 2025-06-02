693f48a0591fe15b3874250e3c4c36a94297c428
krzysztof <krzdur@gmail.com>
2025-06-02 20:55:47 +0200

    add registration and logging backend

--- webapp/main/urls.py
+++ webapp/main/urls.py
@@ -1,10 +1,16 @@
 # Plik do zarządzania ścieżkami w aplikacji. Zawiera listę ścieżek powiązanych z widokami.
 
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),  # Home page (example)
     path('about/', views.about, name='about'),
-    path('interior/', views.interior, name='interior')
+    path('interior/', views.interior, name='interior'),
+    path('register/', views.register, name='register_user'),
+    path('login/', auth_views.LoginView.as_view(template_name='main/registration/login.html'), name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('', include('django.contrib.auth.urls'))
 ]
--- webapp/main/views.py
+++ webapp/main/views.py
@@ -1,6 +1,9 @@
 # Plik do definiowania widoków, które są renderowane za pomocą szablonizatora Jinja oraz wyświetlane w przeglądarce
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import login, logout
 
 # Create your views here.
 def index(request):
@@ -11,3 +14,23 @@ def about(request):
 
 def interior(request):
     return render(request, 'main/interior.html')
+
+def register(request):
+    if request.user.is_authenticated:
+        return redirect('/')  # Redirect logged-in registration
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            messages.success(request, 'Account created successfully!')
+            login(request, user)  # Auto-login after registration
+            return redirect('/')  # Redirect to home or dashboard
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'main/registration/register.html', {'form': form})
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
